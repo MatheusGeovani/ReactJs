cba756f2aac8a120e92a1cfe9abb57be639f4ab9
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-04-21 19:32:37 -0300

    project organo

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,5 +1,6 @@
 import Banner from "./Componentes/Banner";
 import Formulario from "./Componentes/Formulario";
+import ListaSuspensa from "./Componentes/ListaSuspensa";
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario/>
+      <ListaSuspensa/>
     </div>
   );
 }
--- organo/src/Componentes/Formulario/index.js
+++ organo/src/Componentes/Formulario/index.js
@@ -1,26 +1,38 @@
+import { useState } from 'react'
 import CampoTexto from '../CampoTexto'
 import './Formulario.css'
 
 const Formulario = () => {
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+
+    const salvar = (evento) => {
+        evento.preventDefault()
+    }
+
     return(
         <section className="formulario">
-            <form>
+            <form onSubmit={salvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
                 <CampoTexto
                     label="Nome"
-                    // valor={nome}
+                    valor={nome}
                     placeholder="Digite seu nome"
+                    aoAlterado = {valor => setNome(valor)}
                 />
                 <CampoTexto
                     label="Cargo"
-                    // valor={nome}
+                    valor={cargo}
                     placeholder="Digite seu nome"
+                    aoAlterado={valor => setCargo(valor)}
                 />
                 <CampoTexto
                     label="Imagem"
-                    // valor={nome}
+                    valor={imagem}
                     placeholder="Digite seu nome"
+                    aoAlterado={valor => setImagem(valor)}
                 />
                 
             </form>
